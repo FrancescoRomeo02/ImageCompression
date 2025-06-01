977175334c23b46b929a49aceb545decbc3bae80
Francesco <romeo.francesco.2002@gmail.com>
2025-06-02 00:30:07 +0200

    update

--- benchmark.py
+++ benchmark.py
@@ -8,11 +8,21 @@ def benchmark_dct2(sizes):
     custom_times = []
     scipy_times = []
     for N in sizes:
-        matrix = np.random.rand(N, N)
-        start = time.perf_counter()
-        custom_dct2(matrix)
-        custom_times.append(time.perf_counter() - start)
-        start = time.perf_counter()
-        dctn(matrix, norm='ortho')
-        scipy_times.append(time.perf_counter() - start)
+        matrix = np.random.uniform(0, 255, (N, N))
+        
+        # repeat the test 10 times for each size to get an average time
+        custom_time = 0
+        scipy_time = 0
+       
+        for _ in range(10):
+            start_time = time.time()
+            custom_dct2(matrix)
+            custom_time += time.time() - start_time
+            start_time = time.time()
+            dctn(matrix, type=2, norm='ortho')
+            scipy_time += time.time() - start_time
+       
+        custom_times.append(custom_time / 10)
+        scipy_times.append(scipy_time / 10)
+
     return custom_times, scipy_times
--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from benchmark import benchmark_dct2
 from plot_utils import plot_performance
-
+from log_benchmark_csv import save_benchmark_csv
 
 def main():
     sizes = [2**i for i in range(4, 11)]
     custom_times, scipy_times = benchmark_dct2(sizes)
     plot_performance(sizes, custom_times, scipy_times)
-
+    save_benchmark_csv(custom_times, scipy_times, sizes)
 
 if __name__ == '__main__':
     main()
